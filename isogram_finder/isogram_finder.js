35ff81fb1a9eb8402846f474ae43277549055ded
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-05 23:58:25 +0100

    finishes isogram finder and passes test

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,8 +1,28 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
 
+
+
 IsogramFinder.prototype.isIsogram = function () {
+const letters = this.word.split('');
+const lettersLC = letters.map(letter => letter.toLowerCase());
+const letterCount = {};
+let result = true;
+
+for (const letter of lettersLC) {
+  if (letterCount[letter]) {
+    letterCount[letter] += 1;
+  } else {
+    letterCount[letter] = 1;
+  }
+};
+for (const letter in letterCount) {
+  if (letterCount[letter] > 1) {
+    result = false;
+  }
+}
+return result;
 
 }
 
