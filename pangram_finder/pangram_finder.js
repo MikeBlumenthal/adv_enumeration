9a1b4943247d8770d85abd09fa2a64c65f1edefd
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-05 22:48:19 +0100

    finishes pangram finder and passes tests

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const phraseLC = this.phrase.toLowerCase();
+  return this.alphabet.every(letter => phraseLC.includes(letter));
 }
 
 module.exports = PangramFinder;
